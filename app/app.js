283a9f2a384927b811e5fbce21c6450dc6c5f2ca
Chai Phonbopit <phonbopit@gmail.com>
2015-02-17 10:15:50 +0700

    update index.html

--- app/app.js
+++ app/app.js
@@ -1,21 +1,8 @@
 'use strict'
 
 angular.module('myApp', [])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-      .when('/home', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeController'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutController'
-      })
-      .when('/hello', {
-        templateUrl: 'views/hello.html',
-        controller: 'HelloController'
-      })
-      .otherwise({
-        redirectTo: '/home'
-      })
-  }]);
+
+  .controller('MainController', function() {
+    var vm = this;
+    vm.name = 'Chai';
+  });
